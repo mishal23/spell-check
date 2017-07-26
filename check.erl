77c95d43a4a23626d47814386a86fa0c1f55dcac
mishal23 <shahmishal1998@gmail.com>
2017-07-26 22:27:10 +0530

    Improved accuracy and runtime speed

--- check.erl
+++ check.erl
@@ -1,10 +1,12 @@
 -module (check).
 -export ([readfile/1,deletion_edits/1,transposition_edits/1,alteration_edits/1,insertion_edits/1,edits1/1,edits2/1,known/1]).
 
+% Read the whole file and makes a list of each word.
 readfile(FileName) -> {ok,Binary} = file:read_file(FileName),
-                      re:split(binary_to_list(Binary), "[^a-zA-Z]").
+%                      re:split(binary_to_list(Binary), "[^a-zA-Z]").
+                      lists:usort(lists:map(fun(X) -> string:to_lower(binary_to_list(X)) end,(re:split(binary_to_list(Binary),"[^a-zA-Z]")))).
 
-alphabet() ->	"abcdefghijklmnopqrstuvwxyz".
+letters() ->	"abcdefghijklmnopqrstuvwxyz".
 
 % Form list of words by deleting each letter from the word
 % eg. for "hello" -> ["hell","helo","helo","hllo","ello"]
@@ -23,22 +25,22 @@ transposition_edits(_, [_], Edits) ->	Edits;
 transposition_edits(Before, [Current, Next | After], Edits) ->	transposition_edits([Current | Before], [Next | After],[ lists:reverse(Before) ++ [Next, Current] ++ After | Edits]).
 
 
-% Form list of words by changing one letter with each of the alphabet
+% Form list of words by changing one letter with each of the letters
 % eg. for "me" -> ["ma","mb","mc","..","..","ae","be","ce","..",".."]
 alteration_edits(Word) -> alteration_edits([], Word, []).
 
 alteration_edits(_, [], Edits) ->  Edits;
 alteration_edits(Before, [Current | After], Edits) ->  BeforeR = lists:reverse(Before),
-                                                        alteration_edits([Current | Before], After, [BeforeR ++ [X] ++ After || X <- alphabet()] ++ Edits).
+                                                        alteration_edits([Current | Before], After, [BeforeR ++ [X] ++ After || X <- letters()] ++ Edits).
 
 
-% Form list of words by inserting each alphabet in the word at each possible position in the word
+% Form list of words by inserting each letters in the word at each possible position in the word
 % eg. for "me" -> ["mea","meb","mec","...","...","mae","mbe","mce","...","...","ame","bme","cme","...","..."]
-insertion_edits(Word) -> insertion_edits([], Word, [[X] ++ Word || X <- alphabet()]).
+insertion_edits(Word) -> insertion_edits([], Word, [[X] ++ Word || X <- letters()]).
 
 insertion_edits(_, [], Edits) -> Edits;
 insertion_edits(Before, [Current | After], Edits) -> BeforeR = lists:reverse(Before),
-                                                      insertion_edits([Current | Before], After, [BeforeR ++ [Current, X] ++ After || X <- alphabet()] ++ Edits).
+                                                      insertion_edits([Current | Before], After, [BeforeR ++ [Current, X] ++ After || X <- letters()] ++ Edits).
 
 
 % Forms list with all the edits which are possible and filters the elements repeating using "lists:usort"
@@ -50,19 +52,27 @@ edits2(Word) -> lists:usort(lists:foldr(fun(A, AccIn) -> AccIn ++ edits1(A) end,
 
 
 % Compares all the elements of the words obtained from big.txt and the words formed by edits and returns the possible list of words
-known(Word) ->  Words_in_dictionary = readfile("big.txt"),
-                FinalDictionary = lists:map(fun(X) -> string:to_lower(binary_to_list(X)) end,Words_in_dictionary),
+known(Word) ->  FinalDictionary = readfile("big.txt"),
+%                FinalDictionary = lists:map(fun(X) -> string:to_lower(binary_to_list(X)) end,Words_in_dictionary),
                 Error1_words = edits1(string:to_lower(Word)),
-                Error2_words = edits1(string:to_lower(Word)),
-                AppendedList = lists:usort(lists:append(Error1_words,Error2_words)),
-                SuggestedList = lists:filter(fun(X) -> lists:member(X,FinalDictionary) end,AppendedList),
+%                Error2_words = edits1(string:to_lower(Word)),
+%                AppendedList = lists:usort(lists:append(Error1_words,Error2_words)),
+                SuggestedList = lists:filter(fun(X) -> lists:member(X,FinalDictionary) end,Error1_words),
                 case lists:member(string:to_lower(Word),SuggestedList) of
                     true -> Word;
-                    false -> case length(SuggestedList)=:=0 of
-                               true -> io:format("No suggestions found~n"),
-                                        Word;
-                               false -> io:format("Did you mean?~n"),
-                                    SuggestedList
+                    false ->  case length(SuggestedList)=/=0 of
+                               true -> io:format("Did you mean?~n"),
+                                      SuggestedList;
+                               false -> Error2_words = edits2(string:to_lower(Word)), 
+                                        SuggestedList2 = lists:filter(fun(X) -> lists:member(X,FinalDictionary) end,Error2_words),
+                                        case lists:member(string:to_lower(Word),SuggestedList2) of
+                                          true -> Word;
+                                          false ->  case length(SuggestedList2)=:=0 of
+                                                      true -> Word;
+                                                      false -> io:format("Did you mean?~n"), 
+                                                            SuggestedList2
+                                                    end
+                                        end
                               end
                 end.
 
